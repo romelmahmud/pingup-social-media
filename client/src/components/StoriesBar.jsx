a6ce1cae41f21f893cbaa896db60c6c2c5408717
romel <romelmahmud.dev@gmail.com>
2025-10-05 02:02:43 +0600

    stories card list

--- client/src/components/StoriesBar.jsx
+++ client/src/components/StoriesBar.jsx
@@ -1,4 +1,5 @@
 import { Plus } from "lucide-react";
+import moment from "moment/moment";
 import { useEffect, useState } from "react";
 import { dummyStoriesData } from "../assets/assets";
 
@@ -26,8 +27,42 @@ const StoriesBar = () => {
             </p>
           </div>
         </div>
+        {/* story cards list */}
+        {stories.map((story, index) => (
+          <div
+            key={index}
+            className={`relative rounded-lg shadow min-w-30 max-h-40 cursor-pointer hover:shadow-lg transition-all duration-200 bg-gradient-to-b from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95`}
+          >
+            <img
+              src={story.user.profile_picture}
+              alt={`${story.user.full_name} story`}
+              className="absolute size-8 top-3 left-3 z-10 rounded-full ring ring-gray-100 shadow"
+            />
+            <p className="absolute top-18 left-3 text-white/60 text-sm truncate max-w-24">
+              {story.content}
+            </p>
+            <p className="text-white absolute bottom-1 right-2 z-10 text-xs">
+              {moment(story.createdAt).fromNow()}
+            </p>
+            {story.media_type !== "text" && (
+              <div className="absolute inset-0 z-1 rounded-lg bg-black overflow-hidden">
+                {story.media_type === "image" ? (
+                  <img
+                    src={story.media_url}
+                    alt=""
+                    className="h-full w-full object-cover hover:scale-110 transition duration-500 opacity-70 hover:opacity-80"
+                  />
+                ) : (
+                  <video
+                    src={story.media_url}
+                    className="h-full w-full object-cover hover:scale-110 transition duration-500 opacity-70 hover:opacity-80"
+                  />
+                )}
+              </div>
+            )}
+          </div>
+        ))}
       </div>
-      {/* stories cards */}
     </div>
   );
 };
