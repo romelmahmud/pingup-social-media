a59b6ccac64c0e7478abf95b80d77a8ed323ba78
romel <romelmahmud.dev@gmail.com>
2025-10-19 12:50:59 +0600

    refactor:messages:button navigation link

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -31,7 +31,7 @@ const Messages = () => {
               </div>
               <div className="flex flex-col gap-2 mt-4">
                 <button
-                  onClick={() => navigate(`/massages/${user._id}`)}
+                  onClick={() => navigate(`/messages/${user._id}`)}
                   className="size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer gap-1"
                 >
                   <MessageSquare className="w-4 h-4" />
