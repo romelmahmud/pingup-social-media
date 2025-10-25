b53e57505c8ea4a0379cff92329e4d4241efbcc3
romel <romelmahmud.dev@gmail.com>
2025-10-26 00:29:51 +0600

    Profile edit modal:Profile picture

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import { dummyPostsData, dummyUserData } from "../assets/assets";
 import Loading from "../components/Loading";
 import PostCard from "../components/PostCard";
+import ProfileModal from "../components/ProfileModal";
 import UserProfileInfo from "../components/UserProfileInfo";
 const Profile = () => {
   const { profileId } = useParams();
@@ -100,7 +101,7 @@ const Profile = () => {
         )}
       </div>
       {/* Edit profile Modal */}
-      {showEdit && <p>show profile edit</p>}
+      {showEdit && <ProfileModal setShowEdit={setShowEdit} />}
     </div>
   ) : (
     <Loading />
