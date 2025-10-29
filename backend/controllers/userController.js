47df05350d084f2113c87cb007b63f0cac8b8a70
romel <romelmahmud.dev@gmail.com>
2025-10-29 18:07:35 +0600

    backend:intregte imagekit and discover users fuction

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,19 @@
-// update user data using userId
+import fs from "fs";
+import imagekit from "../config/imageKit.js";
 import User from "../models/User.js";
+// update user data using userId
 
 export const updateUserData = async (req, res) => {
   try {
     const { userId } = req.auth();
     const { username, bio, location, full_name } = req.body;
     const tempUser = await User.findById(userId);
+    if (!tempUser) {
+      return res.json({
+        success: false,
+        message: "User not found",
+      });
+    }
     !username && (username = tempUser.username);
     if (tempUser.username !== username) {
       const user = User.findOne({ username });
@@ -20,21 +28,67 @@ export const updateUserData = async (req, res) => {
       location,
       full_name,
     };
+
     const profile = req.files.profile && req.files.profile[0];
     const cover = req.files.cover && req.files.cover[0];
 
-    if (!userId) {
-      return res.json({
-        success: false,
-        message: "User not found",
+    if (profile) {
+      const buffer = fs.readFileSync(profile.path);
+      const response = await imagekit.upload({
+        file: buffer,
+        fileName: profile.originalname,
+        folder: "/users/profile",
       });
+      const url = imagekit.url({
+        path: response.filePath,
+        transformation: [
+          {
+            quality: "auto",
+          },
+          {
+            format: "webp",
+          },
+          {
+            width: 512,
+          },
+        ],
+      });
+      updateData.profile_picture = url;
     }
-    res.json({ success: true, user });
+
+    if (cover) {
+      const buffer = fs.readFileSync(cover.path);
+      const response = await imagekit.upload({
+        file: buffer,
+        fileName: cover1``.originalname,
+        folder: "/users/cover",
+      });
+      const url = imagekit.url({
+        path: response.filePath,
+        transformation: [
+          {
+            quality: "auto",
+          },
+          {
+            format: "webp",
+          },
+          {
+            width: 1280,
+          },
+        ],
+      });
+      updateData.cover_photo = url;
+    }
+    const user = await User.findByIdAndUpdate(userId, updateData, {
+      new: true,
+    });
+
+    res.json({ success: true, user, message: "Profile Updated successfully" });
   } catch (error) {
     console.log(error);
     res.json({
       success: false,
-      message: message.error,
+      message: error.message,
     });
   }
 };
@@ -45,7 +99,7 @@ export const getUserId = async (req, res) => {
   try {
     const { userId } = req.auth();
     const user = await User.findById(userId);
-    if (!userId) {
+    if (!user) {
       return res.json({
         success: false,
         message: "User not found",
@@ -56,7 +110,32 @@ export const getUserId = async (req, res) => {
     console.log(error);
     res.json({
       success: false,
-      message: message.error,
+      message: error.message,
+    });
+  }
+};
+
+// find user using username, location, email, fileName
+
+export const discoverUsers = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { input } = req.body;
+    const allUsers = await User.find({
+      $or: [
+        { username: new RegExp(input, "i") },
+        { email: new RegExp(input, "i") },
+        { full_name: new RegExp(input, "i") },
+        { location: new RegExp(input, "i") },
+      ],
+    });
+    const filteredUsers = allUsers.filter((user) => user._id !== userId);
+    res.json({ success: true, users: filteredUsers });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
     });
   }
 };
