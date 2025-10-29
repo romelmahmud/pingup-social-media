010838c6d27ec66a6f04941e915b91bb00243b3b
romel <romelmahmud.dev@gmail.com>
2025-10-29 23:40:14 +0600

    backend:user routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import express from "express";
 import { serve } from "inngest/express";
 import connectDB from "./config/db.js";
 import { functions, inngest } from "./inngest/index.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 await connectDB();
@@ -16,6 +17,8 @@ app.get("/", (req, res) => {
   res.send("Sever is running");
 });
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.use("api/user", userRouter);
 
 const PORT = process.env.PORT || 4000;
 
